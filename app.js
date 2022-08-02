7c527ea9cfd826ad45a578c454386c4c291d21d1
JinnoVK <pseudo@live.co.uk>
2022-08-02 10:39:55 +0100

    complete :article_id endpoint with error handling

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { getArticleById } = require("./controllers/articles");
 const { getTopics } = require("./controllers/topics");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found!" });
@@ -10,5 +13,17 @@ app.all("*", (req, res, next) => {
 });
 
 /////////////////////////////////////////////
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(404).send({ msg: "Path not found!" });
+  } else next(err);
+});
 
 module.exports = app;
